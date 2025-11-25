04a56366e6197c894391d5334a9311e0c710b796
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-25 19:36:48 +0530

    Reformat vars.py with consistent line endings

--- vars.py
+++ vars.py
@@ -13,13 +13,14 @@ CREDIT = environ.get("CREDIT", "")
 TOTAL_USER = os.environ.get('TOTAL_USERS', '7540570087,7061670859').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7540570087').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7540570087,7061670859').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
 
