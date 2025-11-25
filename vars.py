fab680dae76478e8ce9ff7c613f255a2eb73224c
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-25 16:31:13 +0530

    Update API credentials and owner details

--- vars.py
+++ vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27400172"))
+API_HASH = environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8396814092:AAHtUGEPciF5LdiCo572-TczY3EZwx0Otis")
 
-OWNER = int(environ.get("OWNER", ""))
-CREDIT = environ.get("CREDIT", "")
+OWNER = int(environ.get("OWNER", "7540570087"))
+CREDIT = environ.get("CREDIT", "NOOB")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -20,3 +20,4 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
